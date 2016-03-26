ab1840eab8b013f72a01f27f86f14251e78e7efc
Michal Lomnicki <michal.lomnicki@gmail.com>
2016-03-26 14:31:25 +0100

    Make events compatible with RES
    Provide default event ID and timestamp.
    Add Event#to_h to serialize events

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem 'rails', '>= 5.0.0.beta3', '< 5.1'
 gem 'pg', '~> 0.18'
 gem 'puma'
 gem 'rails_event_store'
-gem 'dry-types'
+gem 'dry-types', github: "dryrb/dry-types"
 gem 'command_bus', github: 'pawelpacana/command_bus'
 
 group :development do
--- lib/events.rb
+++ lib/events.rb
@@ -1,5 +1,30 @@
+require_relative "types"
+
 module Events
-  Event = Class.new(Dry::Types::Value)
+  class Event < Dry::Types::Value
+    def self.inherited(klass)
+      super
+      klass.constructor_type(:schema)
+      klass.attribute :event_id,  Types::UUID.default     { SecureRandom.uuid }
+      klass.attribute :timestamp, Types::DateTime.default { Time.now }
+    end
+
+    def to_h
+      {
+        event_id: event_id,
+        data:     super.except(:event_id, :timestamp),
+        metadata: { timestamp: timestamp }
+      }
+    end
+
+    def data
+      to_h[:data]
+    end
+
+    def ==(other_event)
+      other_event.instance_of?(self.class) && other_event.data == data
+    end
+  end
 
   class PersonFollowed < Event
     attribute :follower_id,        Types::UUID
